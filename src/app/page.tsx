a0df8fdbd3bc813dd2af88eb745c11efbdbcfca3
Joshua Kisnorbo <joshua.kisnorbo@stu.burgmann.act.edu.au>
2024-05-20 15:33:01 +1000

    Now we tell you your browser sucks in your native tongue.
    Started creation and layouts of static pages

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Home</h1>
+        <br />
+        <div className="infoPanes">
+          <div className='leftInfo'>
+            <h2>Characters</h2>
+            Character 1
+          </div>
+          <div className='rightInfo'>
+            <h2>Campaigns</h2>
+            Campaign 1
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
