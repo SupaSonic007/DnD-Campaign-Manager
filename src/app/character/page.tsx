063b2e608f68951c4da6d3ac23b4e6a05cc4dae6
SupaSonic007 <joshuakonline@gmail.com>
2024-11-06 19:48:32 +1100

    Fix nextjs error

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -7,21 +7,11 @@ export default async function Page() {
     const currentUser = await getCurrentUser()
     const chars = currentUser ? await getUserChars(currentUser) : []
 
-    return <main>{<Characters chars={chars} />}</main>;
-}
-
-export async function Characters({
-    chars,
-}: {
-    chars: { id: string; name: string; owner: string; url: string }[];
-}) {
-    return (
-        <>
+    return <main>
             {chars.map((char) => (
                 <ul key={char.id}>
                     <a href={char.url}>{char.name}</a>
                 </ul>
             ))}
-        </>
-    );
+    </main>;
 }
